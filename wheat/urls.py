c0cd9ab810ac7f47108a22b733086b3ed5c9b422
laozhu <ambulance120@163.com>
2020-10-03 00:46:41 +0800

    '2'

--- wheat/urls.py
+++ wheat/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cms', include("apps.cms.urls")),
     path('', include("apps.cms.urls")),
 ]
