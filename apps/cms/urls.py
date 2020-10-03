f3ff907cef0be84962019bfb3e2aa1a759c872fd
laozhu <ambulance120@163.com>
2020-10-03 11:28:00 +0800

    '2'

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -11,7 +11,7 @@ from django.conf import settings
 app_name = 'cms'
 
 urlpatterns = [
-    path("",views.index,name="index"),
+    path("",views.show_goods,name="index"),
     path("imageupdata",views.imageupdata,name="imageupdata"),
     path("writeIndatabase",views.writeIndatabase,name="writeIndatabase"),
     path("shopView",views.shopView,name="shopView"),
@@ -40,5 +40,5 @@ urlpatterns += [
 urlpatterns += [
                   # url(r'^upload', views.uploadImg,name="upload"),
                   # url(r'^show', views.showImg,name="show"),
-              ]
+              ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  这句话是用来指定和映射静态文件
--- wheat/urls.py
+++ wheat/urls.py
@@ -20,4 +20,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.cms.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
